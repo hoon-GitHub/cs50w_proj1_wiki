265b7d9f8a3fcb48be3a053724bd202bea822885
unknown <hoon81@gmail.com>
2024-02-14 11:59:07 -0500

    Search functionality, view and page added

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from . import util
@@ -9,6 +10,7 @@ def index(request):
         "entries": util.list_entries()
     })
 
+# entry display view for the title
 def wiki(request, title):
     entry = util.get_entry(title)
     return render(request, "encyclopedia/wiki.html", {
@@ -16,3 +18,23 @@ def wiki(request, title):
         "entry": entry
     })
 
+# search view
+def search(request):
+    keyword = request.GET.get("q", "")
+    entry = util.get_entry(keyword)
+
+    # if there is a match
+    if not entry == None:
+        return wiki(request, keyword)
+    
+    # if there is not a match, check if there is any substring match(es)
+    else:
+        matches = []
+        for match in util.list_entries():
+            if keyword.lower() in match.lower():
+                matches.append(match)
+            
+        return render(request, "encyclopedia/search.html", {
+            "keyword": keyword,
+            "matches": matches
+        })
